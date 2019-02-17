aa50dc29cf6b9cf7276914c8f95dbabf3d5e66ba
Samuil Ivanov <samuil2001ivanov@gmail.com>
2019-02-17 19:38:38 +0200

    Adding Director and Movie models. Adding admin user.

--- movies_games/movies/models.py
+++ movies_games/movies/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    movie_name = models.CharField(max_length = 100)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.movie_name
+
+class Director(models.Model):
+    movie = models.ForeignKey(Movie, on_delete = models.CASCADE)
+    first_name = models.CharField(max_length = 15, default = "Prakaj")
+    last_name = models.CharField(max_length = 15, default = "Prakaj")
+    age = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.name
