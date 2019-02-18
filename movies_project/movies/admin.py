b4833d47e4000d8755c1c997e85f019440d855af
Samuil Ivanov <samuil2001ivanov@gmail.com>
2019-02-18 20:24:49 +0200

    Adding movies in the director's details in the admin site.

--- movies_project/movies/admin.py
+++ movies_project/movies/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import Director, Movie
 
-admin.site.register(Director)
-admin.site.register(Movie)
+class MovieInline(admin.TabularInline):
+    model = Movie
+
+class DirectorAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields' : ['first_name', 'last_name', 'age']})
+    ]
+    inlines = [MovieInline]
+
+class MovieAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields' : ['title', 'director']})
+    ]
+
+admin.site.register(Director, DirectorAdmin)
+admin.site.register(Movie, MovieAdmin)
