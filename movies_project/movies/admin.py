d5a403c0e465b36a8d975ca9f0f2a8818ad822bd
Samuil Ivanov <samuil2001ivanov@gmail.com>
2019-02-20 23:23:21 +0200

    Adding more fields to the models
    Added genre and rating fields to the Movie model.
    Also added a filter for the genre/rating of the movie.
    Added search functionality to the admin site.

--- movies_project/movies/admin.py
+++ movies_project/movies/admin.py
@@ -9,11 +9,19 @@ class DirectorAdmin(admin.ModelAdmin):
         (None, {'fields' : ['first_name', 'last_name', 'age']})
     ]
     inlines = [MovieInline]
+    list_display = ('first_name', 'last_name', 'age')
+    search_fields = ['first_name', 'last_name', 'age']
 
 class MovieAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None, {'fields' : ['title', 'director']})
+        (None, {'fields' : ['title', 'director']}),
+        ('Description', {'fields' : ['description']}),
+        ('More information', {'fields' : ['genre', 'rating']})
     ]
+    list_display = ('title', 'director', 'genre', 'rating')
+    list_filter = ['genre', 'rating']
+    search_fields = ['title', 'genre', 'rating']
+
 
 admin.site.register(Director, DirectorAdmin)
 admin.site.register(Movie, MovieAdmin)
--- movies_project/movies/models.py
+++ movies_project/movies/models.py
@@ -6,11 +6,14 @@ class Director(models.Model):
     age = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.first_name + self.last_name
+        return self.first_name + " " + self.last_name
 
 class Movie(models.Model):
     director = models.ForeignKey(Director, on_delete = models.CASCADE)
     title = models.CharField(max_length = 100)
+    description = models.TextField(null = True)
+    genre = models.CharField(max_length = 50, null = True)
+    rating = models.CharField(max_length = 10, null = True)
 
     def __str__(self):
         return self.title
