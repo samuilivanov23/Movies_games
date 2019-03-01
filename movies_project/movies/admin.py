b2daae03ac4382a3e4de0d129195193a7fa8be2b
Samuil Ivanov <samuil2001ivanov@gmail.com>
2019-03-01 22:38:43 +0200

    Changing Director to Celebrity Adding admin site changes for the display names of the models.

--- movies_project/movies/admin.py
+++ movies_project/movies/admin.py
@@ -1,27 +1,28 @@
 from django.contrib import admin
-from .models import Director, Movie
+from .models import Celebrity, Movie
 
 class MovieInline(admin.TabularInline):
     model = Movie
 
-class DirectorAdmin(admin.ModelAdmin):
+class CelebrityAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None, {'fields' : ['first_name', 'last_name', 'age']})
+        (None, {'fields' : ['first_name', 'last_name', 'age', 'typeof']})
     ]
     inlines = [MovieInline]
     list_display = ('first_name', 'last_name', 'age')
-    search_fields = ['first_name', 'last_name', 'age']
+    list_filter = ['first_name', 'last_name', 'age', 'typeof']
+    search_fields = ['first_name', 'last_name', 'age', 'typeof']
 
 class MovieAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None, {'fields' : ['title', 'director']}),
+        (None, {'fields' : ['title', 'celebrity']}),
         ('Description', {'fields' : ['description']}),
         ('More information', {'fields' : ['genre', 'rating']})
     ]
-    list_display = ('title', 'director', 'genre', 'rating')
+    list_display = ('title', 'celebrity', 'genre', 'rating')
     list_filter = ['genre', 'rating']
     search_fields = ['title', 'genre', 'rating']
 
 
-admin.site.register(Director, DirectorAdmin)
+admin.site.register(Celebrity, CelebrityAdmin)
 admin.site.register(Movie, MovieAdmin)
--- movies_project/movies/models.py
+++ movies_project/movies/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 
-class Director(models.Model):
+class Celebrity(models.Model):
     first_name = models.CharField(max_length = 15)
     last_name = models.CharField(max_length = 15)
     age = models.IntegerField(default=0)
+    typeof = models.CharField(max_length = 20)
+
+    class Meta:
+        verbose_name = ("Celebrity")
+        verbose_name_plural = ("Celebrities")
 
     def __str__(self):
         return self.first_name + " " + self.last_name
 
 class Movie(models.Model):
-    director = models.ForeignKey(Director, on_delete = models.CASCADE)
+    celebrity = models.ForeignKey(Celebrity, on_delete = models.CASCADE, null = True)
     title = models.CharField(max_length = 100)
     description = models.TextField(null = True)
     genre = models.CharField(max_length = 50, null = True)
